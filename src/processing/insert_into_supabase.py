1e49dee84ced38c0cf87919edca32dde877b2111
andomingos87 <andomingos87@gmail.com>
2025-05-31 19:05:11 -0300

    Update

--- src/processing/insert_into_supabase.py
+++ src/processing/insert_into_supabase.py
@@ -1,9 +1,7 @@
 import os
 import json
 from supabase import create_client
-# from dotenv import load_dotenv
 
-# load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
